4dc807d4721243b599b95405fc19cdfb3cd6eed5
colinzhaoust <1013224545@qq.com>
2018-12-30 20:40:01 -0500

    Add files via upload

--- Multilabel Big Issue Prediction.py
+++ Multilabel Big Issue Prediction.py
@@ -11,6 +11,9 @@ from torch import optim
 import time
 from tqdm import tqdm_notebook as tqdm
 from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from textblob import TextBlob
+import random
 
 with open("users.json", "r") as f:
     users = json.load(f)
@@ -113,46 +116,69 @@ def get_user_aspect_vec(user, criterion):
 
 
 def language_feature_generator(text):
+
+    zero = [0.0, 0.0, 0.0, 0.0]
+
     if len(text) == 0:
-        return [0.0, 0.0]
+        return zero
 
     sent_tokenize_list = sent_tokenize(text)
 
     average_sent_length = 0.0
+    average_sent_subjectivity = 0.0
+    average_sent_polarity = 0.0
+
     count = 0
     for sentence in sent_tokenize_list:
         count += 1
-        average_sent_length += len(sentence)
+        sent_blob = TextBlob(sentence)
+        average_sent_subjectivity += sent_blob.sentiment.subjectivity
+        average_sent_polarity += sent_blob.sentiment.polarity
+        average_sent_length += len(word_tokenize(sentence))
 
-    average_sent_length = float(average_sent_length/count)
+    if count == 0:
+        return zero
 
-    return len(sent_tokenize(text)), average_sent_length
+    average_sent_length = float(average_sent_length / count)
+    average_sent_subjectivity = float(average_sent_subjectivity / count)
+    average_sent_polarity = float(average_sent_polarity / count)
+
+    return len(sent_tokenize(text)), average_sent_length, average_sent_subjectivity, average_sent_polarity
 
 
 def get_user_language_vec(user):
     language_vec = []
 
     if not user.get("opinion_arguments"):
-        return [0.0, 0.0, 0.0]
+        return [0.0 for i in range(4)]
 
     arguments = user["opinion_arguments"]
     # number of opinions
-    language_vec.append(float(len(arguments)))
+    # language_vec.append(float(len(arguments)))
 
     average_sentence_number = 0.0
     average_sentence_length = 0.0
+    average_sentence_sub = 0.0
+    average_sentence_polar = 0.0
+
     opinion_count = 0
     for opinion in arguments:
         opinion_count += 1
-        sent_number, sent_length = language_feature_generator(opinion["opinion text"])
+        sent_number, sent_length, sent_sub, sent_polar = language_feature_generator(opinion["opinion text"])
         average_sentence_number += sent_number
         average_sentence_length += sent_length
+        average_sentence_sub += sent_sub
+        average_sentence_polar += sent_polar
 
     average_sentence_number/opinion_count
     average_sentence_length/opinion_count
+    average_sentence_sub = average_sentence_sub / opinion_count
+    average_sentence_polar = average_sentence_polar / opinion_count
 
     language_vec.append(average_sentence_number)
     language_vec.append(average_sentence_length)
+    language_vec.append(average_sentence_sub)
+    language_vec.append(average_sentence_polar)
 
     return language_vec
 
@@ -206,7 +232,7 @@ def get_training_set(users, target_big_issue, big_issue_list, criterion):
 
         # see[possible[label]] += 1
 
-        # vec.extend(get_user_language_vec(user))
+        vec.extend(get_user_language_vec(user))
 
         if vec and len(label) == 2 * len(target_big_issue):
             # no N/S
@@ -218,9 +244,43 @@ def get_training_set(users, target_big_issue, big_issue_list, criterion):
 
 # print(get_big_issue_vec(users["ahuggies30"], big_issues_list))
 # target_big_issue = ["Progressive Tax", "Estate Tax", "Flat Tax"]
-target_big_issue = ["Progressive Tax", "Flat Tax"]
-xTr, yTr = get_training_set(valid_users, target_big_issue, big_issues_list, criterion)
+target_big_issue = ["Drug Legalization"]#, "Medical Marijuana", "Abortion"]
+unbalanced_xTr, unbalanced_yTr = get_training_set(valid_users, target_big_issue, big_issues_list, criterion)
 # xTr, yTr = get_training_set(valid_users, "Globalization", big_issues_list, criterion)
+xTr = []
+yTr = []
+number_of_0 = 0
+number_of_1 = 0
+number_each = 690
+
+for i in range(len(unbalanced_xTr)):
+    if unbalanced_yTr[i][0] == 0:
+        if number_of_0 < number_each:
+            xTr.append(unbalanced_xTr[i])
+            yTr.append(unbalanced_yTr[i])
+            number_of_0 += 1
+    if unbalanced_yTr[i][0] == 1:
+        if number_of_1 < number_each:
+            xTr.append(unbalanced_xTr[i])
+            yTr.append(unbalanced_yTr[i])
+            number_of_1 += 1
+
+print(len(xTr), len(yTr))
+print(number_of_0, number_of_1)
+
+mask = np.arange(2 * number_each)
+np.random.shuffle(mask)
+
+temp_xTr = []
+temp_yTr = []
+
+for pick in mask:
+    temp_xTr.append(xTr[pick])
+    temp_yTr.append(yTr[pick])
+
+xTr = temp_xTr
+yTr = temp_yTr
+
 print(len(xTr), len(yTr))
 print(len(xTr[0]), len(yTr[0]))
 
@@ -232,7 +292,7 @@ class model(nn.Module):
         self.hidden_dim = hidden_dim
         self.out_dim = out_dim
         self.embeds = nn.Linear(vocab_size, hidden_dim)
-        self.encoder = nn.ReLU(hidden_dim)
+        self.encoder = nn.Linear(hidden_dim, hidden_dim)
 
         self.out = nn.Linear(hidden_dim, out_dim * output_size)
 
@@ -249,10 +309,14 @@ class model(nn.Module):
         #         input_vectors = self.embeds(torch.tensor(input_seq))
 
         input_vectors = self.embeds(input_vectors)
-        hidden = input_vectors
-        input_vectors = input_vectors.unsqueeze(1)
+        # hidden = input_vectors
+        # input_vectors = input_vectors.unsqueeze(1)
         # _, hidden = self.encoder(input_vectors)
         # print(output.size())
+        input_vectors = torch.nn.functional.relu(input_vectors)
+        hidden = self.encoder(input_vectors)
+        hidden = torch.nn.functional.relu(hidden)
+        hidden = self.encoder(hidden)
         output = torch.nn.functional.relu(hidden)
         output = self.out(output)
         predictions = torch.sigmoid(output)
@@ -340,6 +404,7 @@ for epoch in (range(50)):
 m.eval()
 predictions = 0
 correct = 0
+baseline = 0
 
 max_score = 0
 for i in range(int(len(tqdm(test_x)))):
@@ -352,8 +417,12 @@ for i in range(int(len(tqdm(test_x)))):
 
     if test_y[i][0:2] == prediction[0:2]:
         correct += 1
+    if test_y[i][0:2] == [0.0, 1.0]:
+        baseline += 1
     predictions += 1
 
 accuracy = correct / predictions
+baseline = baseline / predictions
 assert 0 <= accuracy <= 1
 print('Accuracy:{}'.format(accuracy))
+print('Baseline:{}'.format(baseline))
