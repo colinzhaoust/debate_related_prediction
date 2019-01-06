93b67ff73cb8e263f6e1fe2372d5fa4451cb6211
colinzhaoust <1013224545@qq.com>
2019-01-05 21:19:13 -0500

    Add files via upload

--- user_aspect_embedding.py
+++ user_aspect_embedding.py
@@ -11,7 +11,7 @@ import random
 import numpy as np
 import math
 import torch
-
+import random
 from gensim.models import Word2Vec
 from sklearn.decomposition import PCA
 from matplotlib import pyplot
@@ -21,71 +21,87 @@ from torch.nn import init
 from torch import optim
 import time
 from tqdm import tqdm_notebook as tqdm
+import itertools
 
+
+def sent_shuffle(sent):
+    all_sents = []
+    total_length = len(sent)
+
+    for i in range(10):
+        random.shuffle(sent)
+        all_sents.append(sent[:])
+
+    return all_sents
+
+
+# sent = ["I", "am", "what", "you", "don't", "expect"]
+# all_sents = sent_shuffle(sent)
+# print()
+# print(all_sents)
+# breakpoint()
 
 with open("users.json", "r") as f:
     users = json.load(f)
 
 sentences = []
 # useful_aspects = [ 'education', 'ethnicity', 'income', 'interested', 'looking', 'number_of_friends',  'party', 'political_ideology', 'president', 'relationship', 'religious_ideology', 'win_ratio', 'gender']
-useful_aspects = ['political_ideology', 'religious_ideology', 'education', 'ethnicity', 'income','interested', 'looking', 'number_of_friends','win_ratio', 'gender']
-
+useful_aspects = ['political_ideology', 'education', 'ethnicity', 'interested', 'gender']
+# , 'religious_ideology'
 
 for user in users.values():
-    sent_list = []
+    user_sent = []
 
     for aspect in useful_aspects:
 
         if user[aspect] == "Not Saying":
             continue
+        else:
+            user_sent.append(aspect + ":" + user[aspect])
 
-        first_index = aspect + ": " + str(user[aspect])
-        this_sent = [first_index]
-        for key in user["big_issues_dict"].keys():
-            if user["big_issues_dict"][key] == "Pro":
-                this_sent.append(key + "-Pro")
-            if user["big_issues_dict"][key] == "Con":
-                this_sent.append(key + "-Con")
-            if user["big_issues_dict"][key] == "Und":
-                this_sent.append(key + "-Und")
-            if user["big_issues_dict"][key] == "N/O":
-                this_sent.append(key + "-N/O")
-            if user["big_issues_dict"][key] == "N/S":
-                continue
+    for key in user["big_issues_dict"].keys():
 
-        if len(this_sent) > 0:
-            sentences.append(this_sent)
+        for choice in ["Pro", "Con"]: # , "Und", "N/O"]:
+            this_sent = user_sent
+            if user["big_issues_dict"][key] == choice:
+                this_sent.append(key + "-" + choice)
+                if len(this_sent) > 0:
+                    sent_list = sent_shuffle(this_sent)
+                    if len(sent_list) > 0:
+                        sentences.extend(sent_list)
+
 
 
 print(len(sentences))
 
-model = Word2Vec(sentences, size=10, window=2)
+model = Word2Vec(sentences, size=10, window=20, sg=0)
 print(model)
 
 words = list(model.wv.vocab)
-print(words)
+# print(words)
 # print(model["Abortion"])
 
 # for word in words:
 #     print(word)
-print(model.wv.most_similar_cosmul(positive=['political_ideology: Libertarian'], topn=15))
-#
-# X = model[model.wv.vocab]
-#
-# pca = PCA(n_components=3)
-# result = pca.fit_transform(X)
-#
-# fig = pyplot.figure(figsize=[25, 20], dpi=300)
-# ax = Axes3D(fig)
-#
-# ax.scatter(result[:, 0], result[:, 1], result[:, 2])
-# #pyplot.scatter(result[:, 0], result[:, 1], result[:, 2])
-#
-# # for i, word in enumerate(words):
-# #     pyplot.annotate(word, xy=(result[i, 0], result[i, 1], result[i, 2]))
-#
+print(model.wv.most_similar_cosmul(positive=['political_ideology:Conservative'], topn=30))
+print(model.wv.most_similar_cosmul(positive=['political_ideology:Liberal'], topn=30))
+
+X = model[model.wv.vocab]
+
+pca = PCA(n_components=3)
+result = pca.fit_transform(X)
+
+fig = pyplot.figure(figsize=[25, 20], dpi=300)
+ax = Axes3D(fig)
+
+ax.scatter(result[:, 0], result[:, 1], result[:, 2])
+#pyplot.scatter(result[:, 0], result[:, 1], result[:, 2])
+
 # for i, word in enumerate(words):
-#      ax.text(result[i, 0], result[i, 1], result[i, 2], word, size=8, color="k")
-#
-# pyplot.show()
+#     pyplot.annotate(word, xy=(result[i, 0], result[i, 1], result[i, 2]))
+
+for i, word in enumerate(words):
+     ax.text(result[i, 0], result[i, 1], result[i, 2], word, size=8, color="k")
+
+pyplot.show()
 
